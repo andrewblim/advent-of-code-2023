6030c12d1fa84ad10f3c82e278f95885e0df2282
Andrew Lim <andrew.b.lim@gmail.com>
2023-12-29 15:46:34 -0500

    Slightly faster but still slow day21 problem2

--- elixir/lib/day21.ex
+++ elixir/lib/day21.ex
@@ -30,44 +30,30 @@ defmodule Day21 do
     end
   end
 
-  # keep positions as a map from {r, c} => sets of {grid_r, grid_c}
-
-  def step2(map, {n_rows, n_cols}, positions, i) do
+  def step2(map, {n_rows, n_cols}, i, points, prev_points \\ MapSet.new(), total1 \\ 0, total2 \\ 0) do
+    if rem(i, 1000) == 0, do: IO.inspect(MapSet.size(points))
     case i do
       0 ->
-        positions
+        total2 + MapSet.size(points)
       _ ->
-        next_positions =
-          for {{r, c}, grid_positions} <- positions,
+        next_points =
+          for {r, c, grid_r, grid_c} <- points,
               {adjacent_r, adjacent_c} <- [{r - 1, c}, {r + 1, c}, {r, c - 1}, {r, c + 1}],
               effective_r = Integer.mod(adjacent_r, n_rows),
               effective_c = Integer.mod(adjacent_c, n_cols),
               Map.get(map, {effective_r, effective_c}) != "#",
-              reduce: %{} do
-            acc ->
-              {offset_r, offset_c} = cond do
-                adjacent_r < 0 -> {-1, 0}
-                adjacent_r >= n_rows -> {1, 0}
-                adjacent_c < 0 -> {0, -1}
-                adjacent_c >= n_cols -> {0, 1}
-                true -> {0, 0}
-              end
-              updated_grid_positions = if {offset_r, offset_c} == {0, 0} do
-                grid_positions
-              else
-                for {grid_r, grid_c} <- grid_positions, into: MapSet.new() do
-                  {grid_r + offset_r, grid_c + offset_c}
-                end
-              end
-              Map.update(
-                acc,
-                {effective_r, effective_c},
-                updated_grid_positions,
-                fn v -> MapSet.union(updated_grid_positions, v) end
-              )
+              into: MapSet.new() do
+            {offset_r, offset_c} = cond do
+              adjacent_r < 0 -> {-1, 0}
+              adjacent_r >= n_rows -> {1, 0}
+              adjacent_c < 0 -> {0, -1}
+              adjacent_c >= n_cols -> {0, 1}
+              true -> {0, 0}
+            end
+            {effective_r, effective_c, grid_r + offset_r, grid_c + offset_c}
           end
-
-        step2(map, {n_rows, n_cols}, next_positions, i - 1)
+          |> MapSet.difference(prev_points)
+        step2(map, {n_rows, n_cols}, i - 1, next_points, points, total2 + MapSet.size(points), total1)
     end
   end
 
@@ -79,10 +65,7 @@ defmodule Day21 do
 
   def problem2(input \\ "data/day21.txt", type \\ :file, n \\ 64) do
     {map, {n_rows, n_cols}} = read_input(input, type)
-    start = Enum.find(map, fn {_, v} -> v == "S" end) |> elem(0)
-    step2(map, {n_rows, n_cols}, %{start => MapSet.new([{0, 0}])}, n)
-    |> Map.values()
-    |> Enum.map(&MapSet.size/1)
-    |> Enum.sum()
+    {start_x, start_y} = Enum.find(map, fn {_, v} -> v == "S" end) |> elem(0)
+    step2(map, {n_rows, n_cols}, n, MapSet.new([{start_x, start_y, 0, 0}]))
   end
 end
